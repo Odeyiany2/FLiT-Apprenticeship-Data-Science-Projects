f2bb1a7c614a74746e531986c317fd44dac2e1a7
Miriam <108756842+Odeyiany2@users.noreply.github.com>
2023-12-21 22:06:36 +0100

    Delete Project_2 Hotel-Reviews-Analysis-Tableau/SQL-Scripts directory

--- Project_2 Hotel-Reviews-Analysis-Tableau/SQL-Scripts/SQLQuery2-City-Hotels.sql
+++
@@ -1,9 +0,0 @@
--- creating a new table that contains only Resort hotel for each year
-with H2 as (
-select *
-from dbo.[hotel-reviews - hotel_bookings]
-where hotel not like 'Resort Hotel'
-)
-
-select *
-from H2
