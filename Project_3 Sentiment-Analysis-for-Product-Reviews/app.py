544e63842e92778a1d8cd7528db8168534e174db
Miriam <108756842+Odeyiany2@users.noreply.github.com>
2024-03-30 14:45:47 +0000

    Updated

--- Project_3 Sentiment-Analysis-for-Product-Reviews/app.py
+++ Project_3 Sentiment-Analysis-for-Product-Reviews/app.py
@@ -12,20 +12,21 @@ st.markdown("# 😊🙁 Amazon Reviews Sentiment Analyzer")
 model = joblib.load("Amazon_Sentiment_Analysis_Model.joblib")
 
 option = st.selectbox(
-    'What would you like to analyze?',
-    ('A text', 'A csv file'))
+    "What would you like to analyze?",
+    ("A text", "A csv file"))
 
-if option=='A text':
-    title = st.text_area('English Text', 'Enter your english text here?')
-    if st.button('Predict'):
+if option=="A text"
+    title = st.text_area("Enter your english text here?")
+    if st.button("Predict"):
         start = time.time()
         cv = CountVectorizer(ngram_range = (1,2))
         text = cv.fit_transform([title])
         prediction = model.predict(text)
         end = time.time()
-        st.write(f"Prediction time taken: ", round(end-start, 2))
-        title = st.text_area(label='Output', value=translation, height=200, label_visibility='hidden')
+        st.write("Prediction time taken: ", round(end-start, 2), "seconds.")
+        title = st.text_area(label="Output", value=translation, height=200, label_visibility="hidden")
 
+elif option == "A csv file"
 
 
 
