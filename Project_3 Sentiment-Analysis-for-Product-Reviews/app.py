965278bb1abb57fa3b701634dcac047689d132ae
Miriam <108756842+Odeyiany2@users.noreply.github.com>
2024-03-30 15:19:03 +0000

    Updated deployment file

--- Project_3 Sentiment-Analysis-for-Product-Reviews/app.py
+++ Project_3 Sentiment-Analysis-for-Product-Reviews/app.py
@@ -31,14 +31,7 @@ elif option == "A csv file":
     file = st.file_uploader("Upload a csv file")
     if st.button("Predict"):
         start = time.time()
-        cv = CountVectorizer(ngram_range = (1,2))
-        X_text = cv.fit_transform(file)
-        prediction = model.predict(X_text)
-        end = time.time()
-        st.write("Prediction time taken: ", round(end-start, 2), "seconds.")
-        st.write("Predicted Sentiment is: ", prediction)
-        title = st.text_area(label="Output", value=translation, height=200, label_visibility="hidden")
-
+        
 
 
 
