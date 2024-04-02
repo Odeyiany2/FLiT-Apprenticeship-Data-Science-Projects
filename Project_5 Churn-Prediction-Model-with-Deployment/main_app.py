dee3f5142a6bf6143fea876b97e15cacda8f43c1
Miriam <108756842+Odeyiany2@users.noreply.github.com>
2024-04-02 17:17:53 +0000

    Updated

--- Project_5 Churn-Prediction-Model-with-Deployment/main_app.py
+++ Project_5 Churn-Prediction-Model-with-Deployment/main_app.py
@@ -3,4 +3,20 @@ import pandas as pd
 import joblib
 
 
-model = joblib.load(
+st.set_page_config(page_title="Customer Churn Prediction App", page_icon="😊🙁")
+
+st.markdown("# 😊🙁 Customer Churn Prediction")
+
+
+
+
+
+
+
+
+
+
+# load the models 
+model = joblib.load('Churn_Prediction_Model.joblib')
+model_2 = joblib.load("Telecom_Churn_Prediction_Model.joblib")
+
