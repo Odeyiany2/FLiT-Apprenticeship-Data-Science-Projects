9d919fd37e14b2a35c865f9cc90bd867dc00b6b0
Odeyiany2 <miriamodeyianypeter@gmail.com>
2024-04-06 11:35:05 +0100

    Updated Files for Deployment of model

--- Project_3 Sentiment-Analysis-for-Product-Reviews/app.py
+++ Project_3 Sentiment-Analysis-for-Product-Reviews/app.py
@@ -1,45 +1,70 @@
 import streamlit as st
 import time
 import pandas as pd
-import joblib
+#import joblib
 import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.feature_extraction.text import CountVectorizer
+# from nltk.corpus import stopwords
+# from nltk.stem import WordNetLemmatizer
+from textblob import TextBlob
+#import re 
+import cleantext
 
 st.set_page_config(page_title="Amazon Reviews Sentiment Analyzer", page_icon="😊🙁")
 
 st.markdown("# 😊🙁 Amazon Reviews Sentiment Analyzer")
 
 #load model
-model = joblib.load("Amazon_Sentiment_Analysis_Model.joblib")
+#model = joblib.load("Amazon_Sentiment_Analysis_Model.joblib")
 
 option = st.selectbox(
     "What would you like to analyze?",
     ("A text", "A csv file"))
 
-if option=="A text"
+if option=='A text':
     title = st.text_area("Enter your english text here?")
+    #function for preprocessing 
+    def score(x):
+        blob1 = TextBlob(x)
+        return blob1.sentiment.polarity
+
+    #function for analysis
+    def analyze(x):
+        if x >= 0.5:
+            return "Positive"
+        elif x <= -0.5:
+            return "Negative"
+        else:
+            return "Neutral"
     if st.button("Predict"):
         start = time.time()
-        cv = CountVectorizer(ngram_range = (1,2))
-        text = cv.fit_transform([title])
-        prediction = model.predict(text)
+        #cv = CountVectorizer(ngram_range = (1,2))
+        text_score = title.apply(score)
+        prediction = text_score.apply(analyze)
         end = time.time()
         st.write("Prediction time taken: ", round(end-start, 2), "seconds.")
         st.write("Predicted Sentiment is: ", prediction)
-        title = st.text_area(label="Output", value=translation, height=200, label_visibility="hidden")
+    
 
 elif option == "A csv file":
     file = st.file_uploader("Upload a csv file")
     #creating a function to perform our preprocessing of data
-    def data_transform(data):
-        corpus = [] 
-        for item in data: 
-            new_item = re.sub('[^a-zA-Z]',' ',str(item)) 
-            new_item = new_item.lower() 
-            new_item = new_item.split() 
-            new_item = [wn.lemmatize(word) for word in new_item if word not in set(stopwords.words('english'))] 
-            corpus.append(' '.join(str(x) for x in new_item)) 
-        return corpus
+    # # creating an object of WordNetLemmatizer
+    # wn = WordNetLemmatizer()
+    # def data_transform(data):
+    #     corpus = [] 
+    #     for item in data: 
+    #         new_item = re.sub('[^a-zA-Z]',' ',str(item)) 
+    #         new_item = new_item.lower() 
+    #         new_item = new_item.split() 
+    #         new_item = [wn.lemmatize(word) for word in new_item if word not in set(stopwords.words('english'))] 
+    #         corpus.append(' '.join(str(x) for x in new_item)) 
+    #     return corpus
+    
+    #function for preprocessing 
+    def score(x):
+        blob1 = TextBlob(x)
+        return blob1.sentiment.polarity
 
     #function for analysis
     def analyze(x):
@@ -53,10 +78,10 @@ elif option == "A csv file":
     if st.button("Predict"):
         start = time.time()
         df = pd.read_csv(file)
-        corpus = data_transform(df["reviews"])
-        cv = CountVectorizer(ngram_range = (1,2))
-        new_corpus = cv.fit_transform(corpus)
-        df["sentiment_score"] = model.predict(new_corpus)
+        #corpus = data_transform(df["reviews"])
+        #cv = CountVectorizer(ngram_range = (1,2))
+        #new_corpus = cv.fit_transform(corpus)
+        df["sentiment_score"] = df["reviews"].apply(score)
         df["sentiment_analysis"] = df["sentiment_score"].apply(analyze)
         end = time.time()
         st.write(df.head())
@@ -96,6 +121,9 @@ elif option == "A csv file":
                     file_name=sentiment_chart,
                     mime="image/png"
                   )
+else:
+    st.write("Nothing was selected")
+
 
 
 
